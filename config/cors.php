0526c3f20bf38960828b52d128f2bc11ab7cbef0
Thi Beng <beng@example.com>
2026-06-14 22:51:38 +0700

    fix: allow cors for production

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://meeting-logs-frontend.vercel.app'],
-
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
